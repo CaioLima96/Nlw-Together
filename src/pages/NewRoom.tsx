45cc3f2123a16487fcf84a4ef5efd01ce5702de7
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-06-23 17:10:59 -0300

    85% da aula 02 feita

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,12 +1,17 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../App'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 
-import '../styles/auth.scss'
 import { Button } from '../components/Button'
 
+import '../styles/auth.scss'
+
 export function NewRoom() {
+    const { user } = useContext(AuthContext);
+
     return (
         <div id="page-auth">
             <aside>
@@ -18,6 +23,7 @@ export function NewRoom() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Logo"/>
+                    <h1>{user?.name}</h1>
                     <h2>Criar uma nova sala</h2>
                     <form>
                         <input type="text" placeholder="Nome da sala"/>
