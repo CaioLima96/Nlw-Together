b111509ddc3a1e9000b25e2fa74d5c0c85026c18
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-06-27 01:06:11 -0300

    30% da aula 05 feita

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import cx from 'classnames';
 
 import '../styles/question.scss'
 
@@ -9,15 +10,19 @@ type QuestionsProps = {
         avatar: string;
     };
     children?: ReactNode;
+    isAnswered?: boolean;
+    isHighLighted?: boolean;
 }
 
 export function Question({
     content,
     author,
+    isAnswered = false,
+    isHighLighted = false,
     children,
 }: QuestionsProps) {
     return (
-        <div className="question">
+        <div className={cx('question', {answered: isAnswered}, {highLighted: isHighLighted},)}>
             <p>{content}</p>
             <footer>
                 <div className="user-info">
--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -8,8 +8,8 @@ type FirebaseQuestions = Record<string, {
         avatar: string;
     }
     content: string;
-    isAnswered: boolean;
     isHighLighted: boolean;
+    isAnswered: boolean;
     likes: Record<string, {
         authorId: string;
     }>
@@ -22,8 +22,8 @@ type QuestionType = {
         avatar: string;
     }
     content: string;
-    isAnswered: boolean;
     isHighLighted: boolean;
+    isAnswered: boolean;
     likeCount: number;
     likeId: string | undefined;
 }
